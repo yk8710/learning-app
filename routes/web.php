45497093a7a439d0c9c0ece5ad75ea6c624602fa
yk8710 <y.kaneko198710@gmail.com>
2022-09-27 18:17:25 +0900

    コントローラー作成、ルーティング

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/sample/{id}', [\App\Http\Controllers\Sample\IndexController::class, 'showId']);
