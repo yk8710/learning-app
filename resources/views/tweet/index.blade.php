9059c7a0622e6d696ddd0aed6ec21ffbe5258288
yk8710 <y.kaneko198710@gmail.com>
2022-09-28 17:55:37 +0900

    投稿フォーム作成

--- resources/views/tweet/index.blade.php
+++ resources/views/tweet/index.blade.php
@@ -9,6 +9,16 @@
 <body>
   <h1>つぶやきアプリ</h1>
   <div>
+    <p>投稿フォーム</p>
+    <form action="{{ route('tweet.create') }}" method="post">
+      @csrf
+      <label for="tweet-content">つぶやき</label>
+      <span>140文字まで</span>
+      <textarea id="tweet-content" type="text" name="tweet" placeholder="つぶやきを入力"></textarea>
+      <button type="submit">投稿</button>
+    </form>
+  </div>
+  <div>
     @foreach($tweets as $tweet)
       <p>{{ $tweet->content }}</p>
     @endforeach
