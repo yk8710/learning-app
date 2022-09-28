fa79782ab3e5e6437a8f2d57e0d62cf58d2435da
yk8710 <y.kaneko198710@gmail.com>
2022-09-28 18:11:32 +0900

    バリデーションの日本語化

--- resources/views/tweet/index.blade.php
+++ resources/views/tweet/index.blade.php
@@ -15,6 +15,9 @@
       <label for="tweet-content">つぶやき</label>
       <span>140文字まで</span>
       <textarea id="tweet-content" type="text" name="tweet" placeholder="つぶやきを入力"></textarea>
+      @error('tweet')
+      <p style="color: red;">{{ $message }}</p>
+      @enderror
       <button type="submit">投稿</button>
     </form>
   </div>
