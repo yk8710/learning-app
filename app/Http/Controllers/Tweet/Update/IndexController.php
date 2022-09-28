3af751d92fb4f26328eb13414bf75b4e1eff8097
yk8710 <y.kaneko198710@gmail.com>
2022-09-28 19:46:03 +0900

    コントローラーの記述

--- app/Http/Controllers/Tweet/Update/IndexController.php
+++ app/Http/Controllers/Tweet/Update/IndexController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Tweet\Update;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Tweet;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class IndexController extends Controller
 {
@@ -15,6 +17,8 @@ class IndexController extends Controller
      */
     public function __invoke(Request $request)
     {
-        //
+        $tweetId = (int) $request->route('tweetId');
+        $tweet = Tweet::where('id', $tweetId)->firstOrFail();
+        dd($tweet);
     }
 }
