31327082eb12b9b3fd4e6adc5314b18ccb7cf735
yk8710 <y.kaneko198710@gmail.com>
2022-09-29 15:52:46 +0900

    編集内容の更新処理

--- app/Http/Controllers/Tweet/Update/PutController.php
+++ app/Http/Controllers/Tweet/Update/PutController.php
@@ -4,17 +4,19 @@ namespace App\Http\Controllers\Tweet\Update;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Request\Tweet\UpdateRequest;
+use App\Model\Tweet;
 
 class PutController extends Controller
 {
-    /**
-     * Handle the incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function __invoke(Request $request)
+    
+    public function __invoke(UpdateRequest $request)
     {
-        //
+        $tweet = Tweet::where('id', $request->id())->firstOrFail();
+        $tweet->content = request->tweet();
+        $tweet->save();
+        return redirect()
+            ->route('tweet.update.index', ['tweetId' => $tweet->id])
+            ->with('feedback.success', "つぶやきを編集しました");
     }
 }
--- resources/views/tweet/update.blade.php
+++ resources/views/tweet/update.blade.php
@@ -11,6 +11,9 @@
   <div>
     <a href="{{ route('tweet.index') }}">< 戻る</a>
     <p>投稿フォーム</p>
+    @if (session('feedback.success'))
+      <p style="color: green">{{ session('feedback.success') }}</p>
+    @endif
     <form action="{{ route('tweet.update.put', ['tweetId' => $tweet->id]) }}" method="post">
       @method('PUT')
       @csrf
