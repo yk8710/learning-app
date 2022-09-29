36f34fa826fc82d233da798d137f9f0201787fd7
yk8710 <y.kaneko198710@gmail.com>
2022-09-29 18:18:13 +0900

    削除機能実装

--- app/Http/Controllers/Tweet/DeleteController.php
+++ app/Http/Controllers/Tweet/DeleteController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Tweet;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Tweet;
 
 class DeleteController extends Controller
 {
@@ -15,6 +16,11 @@ class DeleteController extends Controller
      */
     public function __invoke(Request $request)
     {
-        //
+        $tweetId = (int) $request->route('tweetId');
+        $tweet = Tweet::where('id', $tweetId)->firstOrFail();
+        $tweet->delete();
+        return redirect()
+            ->route('tweet.index')
+            ->with('feedback.success', "つぶやきを削除しました");
     }
 }
