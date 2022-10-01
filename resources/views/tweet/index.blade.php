e5221241aed089ecf379f56ce666c2436cf7515e
yk8710 <y.kaneko198710@gmail.com>
2022-10-01 11:54:25 +0900

    非ログイン時に投稿フォームを表示しない実装

--- resources/views/tweet/index.blade.php
+++ resources/views/tweet/index.blade.php
@@ -8,6 +8,7 @@
 </head>
 <body>
   <h1>つぶやきアプリ</h1>
+@auth
   <div>
     <p>投稿フォーム</p>
     @if (session('feedback.success'))
@@ -24,6 +25,7 @@
       <button type="submit">投稿</button>
     </form>
   </div>
+@endauth
   <div>
     @foreach($tweets as $tweet)
       <details>
--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,22 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Sample
+Route::get('/sample', [\App\Http\Controllers\Sample\IndexController::class, 'show']);
+Route::get('/sample/{id}', [\App\Http\Controllers\Sample\IndexController::class, 'showId']);
+
+
+// Tweet
+Route::get('/tweet', \App\Http\Controllers\Tweet\IndexController::class)->name('tweet.index');
+Route::post('/tweet/create', \App\Http\Controllers\Tweet\CreateController::class)
+    ->middleware('auth')
+    ->name('tweet.create');
+Route::get('/tweet/update/{tweetId}', \App\Http\Controllers\Tweet\Update\IndexController::class)->name('tweet.update.index');
+Route::put('/tweet/update/{tweetId}', \App\Http\Controllers\Tweet\Update\PutController::class)->name('tweet.update.put');
+Route::delete('/tweet/delete/{tweetId}', \App\Http\Controllers\Tweet\DeleteController::class)->name('tweet.delete');
+
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
