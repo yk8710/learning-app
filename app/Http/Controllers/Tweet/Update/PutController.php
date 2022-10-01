c26898393075d10b1b61675b7cc71b6555569561
yk8710 <y.kaneko198710@gmail.com>
2022-10-01 16:35:21 +0900

    自分の投稿だけを編集、削除可能にする実装

--- app/Http/Controllers/Tweet/Update/PutController.php
+++ app/Http/Controllers/Tweet/Update/PutController.php
@@ -6,12 +6,17 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Requests\Tweet\UpdateRequest;
 use App\Models\Tweet;
+use App\Services\TweetService;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 class PutController extends Controller
 {
     
-    public function __invoke(UpdateRequest $request)
+    public function __invoke(UpdateRequest $request, TweetService $tweetService)
     {
+        if (!$tweetService->checkOwnTweet($request->user()->id, $request->id())) {
+            throw new AccessDeniedHttpException();
+        }
         $tweet = Tweet::where('id', $request->id())->firstOrFail();
         $tweet->content = $request->tweet();
         $tweet->save();
