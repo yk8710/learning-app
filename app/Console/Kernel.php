9a10f4fe5b37a1f028c9f7d5d4aec5b4a1aaaa89
yk8710 <y.kaneko198710@gmail.com>
2022-10-04 14:42:17 +0900

    スケジューラーを実行

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,17 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        //毎分
+        $schedule->command('sample-command')->everyMinute()
+            ->emailOutputTo('hello@example.com');
+        //毎時
+        $schedule->command('sample-command')->hourly();
+        //毎時8分
+        $schedule->command('sample-command')->daily();
+        //毎日13時
+        $schedule->command('sample-command')->dailyAt('13:00');
+        //毎日3:15分(cron表記)
+        $schedule->command('sample-command')->cron('15 3 * * *');
     }
 
     /**
